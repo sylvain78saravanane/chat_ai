d92392b00cf8dbccb5c405efd94aa792dd8bd26f
Sylvain SARAVANANE <sylvainsinou@yahoo.fr>
2024-12-08 13:01:27 +0100

    new version

--- src/main/java/com/chatbot/chat_ai/controller/MyController.java
+++ src/main/java/com/chatbot/chat_ai/controller/MyController.java
@@ -1,6 +1,5 @@
 package com.chatbot.chat_ai.controller;
 
-
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,20 +20,5 @@ public class MyController {
                 .call()
                 .content();
     }
-
-    // private final ChatClient chatClient;
-    //
-    // public MyController(ChatClient.Builder chatClientBuilder) {
-    //     this.chatClient = chatClientBuilder.build();
-    // }
-//
-    // @GetMapping("/ai")
-    // String generation(String userInput) {
-    //     return this.chatClient.prompt()
-    //             .user(userInput)
-    //             .call()
-    //             .content();
-    // }
-
 
 }
