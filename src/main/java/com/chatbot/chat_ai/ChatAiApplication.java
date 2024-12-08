3e9b46de8f86b3c7df3afc9a0df6346096b1db20
Sylvain SARAVANANE <sylvainsinou@yahoo.fr>
2024-12-08 12:58:51 +0100

    new version

--- src/main/java/com/chatbot/chat_ai/ChatAiApplication.java
+++ src/main/java/com/chatbot/chat_ai/ChatAiApplication.java
@@ -1,13 +1,16 @@
 package com.chatbot.chat_ai;
 
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatAiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChatAiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAiApplication.class, args);
+    }
 
 }
+
+// TODO : Mettre en place et vérifier le file : application properties
--- src/main/java/com/chatbot/chat_ai/controller/MyController.java
+++ src/main/java/com/chatbot/chat_ai/controller/MyController.java
@@ -1,2 +1,40 @@
-package com.chatbot.chat_ai.controller;public class MyController {
+package com.chatbot.chat_ai.controller;
+
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    private final ChatClient chatClient;
+
+    public MyController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/ai")
+    String generate(String userInput) {
+        return this.chatClient.prompt()
+                .user(userInput)
+                .call()
+                .content();
+    }
+
+    // private final ChatClient chatClient;
+    //
+    // public MyController(ChatClient.Builder chatClientBuilder) {
+    //     this.chatClient = chatClientBuilder.build();
+    // }
+//
+    // @GetMapping("/ai")
+    // String generation(String userInput) {
+    //     return this.chatClient.prompt()
+    //             .user(userInput)
+    //             .call()
+    //             .content();
+    // }
+
+
 }
